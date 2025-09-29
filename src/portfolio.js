a018fa8d2499513f75210bfd8e7255e251ed6c50
UDM2202 <118545652+UDM2202@users.noreply.github.com>
2025-09-29 14:30:25 +0100

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -278,7 +278,7 @@ const bigProjects = {
 const achievementSection = {
   title: emoji("Achievements And Certifications 🏆 "),
   subtitle:
-    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+    "Achievements, Certifications, and Some Cool Stuff that I have done !",
 
   achievementsCards: [
     {
